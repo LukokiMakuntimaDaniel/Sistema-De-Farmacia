604ae2bb466abcf3033bb137b0cbf46bc4f36af0
lukoki Makuntima Daniel <lukokimakuntimadaniel@gmail.com>
2023-01-02 19:12:43 +0100

    farmacia

---
+++ JS/testEditar.js
@@ -0,0 +1,9 @@
+function text(test) {
+    var text = document.getElementById('exampleModalLabel')
+    return text.innerText = '' + test
+}
+
+function textCadastro(txt) {
+    var text = document.getElementById('exampleModalLabel')
+    return text.innerText = '' + txt
+}
